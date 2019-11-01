3d4bca53daa5d46ad2c38c3ab9e661367e0950f7
Michael Ajami <michaelajami@gmail.com>
2019-11-01 20:42:46 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Michael Ajami on 01/11/2019.
+//  Copyright © 2019 Michael Ajami. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+
+}
